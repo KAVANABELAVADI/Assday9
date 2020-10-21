3a53ce92d9ae87c6494078a17bf5b9ef117b03c9
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-21 08:02:35 +0530

    Using Switch Case

--- empWage.sh
+++ empWage.sh
@@ -3,17 +3,25 @@ EMPLOYEE_RATE_PER_HOUR=20
 FULL_TIME=8
 PART_TIME=8
 
+workDonePerDay=0
+
+getWorkDonePerDay()
+{
+
+        case $1 in
+        0) workDonePerDay=$PART_TIME;;
+        1) workDonePerDay=$FULL_TIME;;
+        *) workDonePerDay=0;
+        esac
+        echo $workDonePerDay; #RETURN STATEMENT
+}
+
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 attendance=$((RANDOM%2))
         if [ $attendance -eq 1 ]
         then
-	emptime=$((RANDOM%2))
-	if [ $emptime == 0 ]
-	then
-	  dailyWage=$((PART_TIME*EMPLOYEE_RATE_PER_HOUR));
-	else
-	   dailyWage=$((FULL_TIME*EMPLOYEE_RATE_PER_HOUR));
-	fi
+	     workDonePerDay=$( getWorkDonePerDay $((RANDOM%2)) );
+	     dailyWage=$((workDonePerDay*EMPLOYEE_RATE_PER_HOUR));
         else
           att="Absent"
         fi
