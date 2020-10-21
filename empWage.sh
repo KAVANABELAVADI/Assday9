174b7b20008e496c616428a0b44716fefa3fe146
KAVANABELAVADI <50482786+KAVANABELAVADI@users.noreply.github.com>
2020-10-21 07:44:49 +0530

    Emp Wage for Full Time

--- empWage.sh
+++ empWage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -x
-
+EMPLOYEE_RATE_PER_HOUR=20
+FULL_TIME=8
 
 echo "Welcome to Employee Wage Computation Program on Master Branch"
 attendance=$((RANDOM%2))
         if [ $attendance -eq 1 ]
         then
-          att="present"
+	dailyWage=$((FULL_TIME*EMPLOYEE_RATE_PER_HOUR));
         else
           att="Absent"
         fi
